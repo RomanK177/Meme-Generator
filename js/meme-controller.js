cdfb9d38679b4f84fa19b064fbfb9695085db380
RomanK <romanklebanov17@gmail.com>
2020-10-21 19:00:58 +0300

    onImgClk

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,1 +1,33 @@
-'use strict';
+"use strict";
+var gCanvas;
+var gCtx;
+
+function onInit() {
+  gCanvas = document.querySelector("#my-canvas");
+  gCtx = gCanvas.getContext("2d");
+  addImages();
+  renderImgs();
+  drawImgFromlocal();
+}
+
+function renderImgs() {
+  let images = getImgs();
+  // console.log(images)
+  var strHtmls = images.map(function (img) {
+    return `
+    <img onclick="onImgClk(this, ${img.id})"  src=${img.url} alt="" />
+        `;
+  });
+
+  document.querySelector(".gallery-images").innerHTML = strHtmls.join("");
+}
+function drawImgFromlocal(id) {
+  var img = new Image();
+  img.src = "./" + getImgById(id).url;
+  img.onload = () => {
+    gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height); //img,x,y,xend,yend
+  };
+}
+function onImgClk(ev, imgId) {
+  drawImgFromlocal(imgId);
+}
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,1 +1,55 @@
-'use strict';
+"use strict";
+
+var gKeywords = { happy: 0, animal: 0 };
+
+var gImgs = [];
+var gMeme = {
+  selectedImgId: undefined,
+  selectedLineIdx: undefined,
+
+  lines: [
+    {
+      text: undefined,
+      size: 20,
+      align: "center",
+      color: "red",
+    },
+  ],
+};
+var gId = 0;
+
+function addImg(url) {
+  return {
+    id: ++gId,
+    url,
+  };
+}
+function addImages() {
+  gImgs.push(addImg("img/1.jpg"));
+  gImgs.push(addImg("img/2.jpg"));
+  gImgs.push(addImg("img/3.jpg"));
+  gImgs.push(addImg("img/4.jpg"));
+  gImgs.push(addImg("img/5.jpg"));
+  gImgs.push(addImg("img/6.jpg"));
+  gImgs.push(addImg("img/7.jpg"));
+  gImgs.push(addImg("img/8.jpg"));
+  gImgs.push(addImg("img/9.jpg"));
+  gImgs.push(addImg("img/10.jpg"));
+  gImgs.push(addImg("img/11.jpg"));
+  gImgs.push(addImg("img/12.jpg"));
+  gImgs.push(addImg("img/13.jpg"));
+  gImgs.push(addImg("img/14.jpg"));
+  gImgs.push(addImg("img/15.jpg"));
+  gImgs.push(addImg("img/16.jpg"));
+  gImgs.push(addImg("img/17.jpg"));
+  gImgs.push(addImg("img/18.jpg"));
+}
+function getImgs() {
+  return gImgs;
+}
+function getImgById(imgId = 1) {
+  var img = gImgs.find(function (img) {
+    return imgId === img.id;
+  });
+  return img;
+}
